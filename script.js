2becc602585484b3bef967444c8efdd7e75ec53a
Oxamyt <dentez.danil@gmail.com>
2024-02-25 19:34:43 +0200

    create-binaryTreeFunc-insert

---
+++ script.js
@@ -0,0 +1,68 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.array = array;
+    this.root = null;
+  }
+
+  buildTree(array, start, end) {
+    if (start > end) {
+      return null;
+    }
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(array[mid]);
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+
+    this.root = root;
+    return root;
+  }
+
+  insert(value) {
+    this.root = this.insertRec(this.root, value);
+  }
+
+  insertRec(root, value) {
+    if (root === null) {
+      root = new Node(value);
+      return root;
+    }
+
+    if (value < root.data) {
+      root.left = this.insertRec(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.insertRec(root.right, value);
+    }
+
+    return root;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const array = [1, 4, 5, 7, 8, 9, 23, 67, 6345];
+
+const end = array.length - 1;
+
+const binary = new Tree(array);
+binary.buildTree(array, 0, end);
+binary.insert(555);
+prettyPrint(binary.root);
